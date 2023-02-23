da6436474db7ec0f0efcc43b0932c2ba5daf40c8
Bekhzod <bekhzodaliakbarov@gmail.com>
2023-02-23 08:53:55 +0500

    done

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,22 @@
 import React, { useRef, useEffect } from "react";
 import { mount } from "marketing/MarketingApp";
+import { useHistory } from "react-router-dom";
 
 const MarketingApp = () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathName }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathName) {
+          history.push(nextPathName);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
